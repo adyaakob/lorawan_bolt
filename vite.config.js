b52efae33e262e563fbb3825daf8faf86f14f489
adyaakob <andylie2004@gmail.com>
2024-12-20 01:16:16 +0800

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,13 @@ export default defineConfig({
         main: 'index.html',
         evaluation_plan: 'pages/evaluation_plan.html',
         evaluation_report: 'pages/evaluation_report.html',
-        evaluation_procedure: 'pages/evaluation_procedure.html'
+        evaluation_procedure: 'pages/evaluation_procedure.html',
+        pre_site_visit_checklist: 'pages/pre_site_visit_checklist.html',
+        team_members: 'pages/team_members.html',
+        project_cost_estimation: 'pages/project_cost_estimation.html',
+        project_implementation_timeline: 'pages/project_implementation_timeline.html',
+        test_setup: 'pages/test_setup.html',
+        map: 'pages/map.html'
       }
     }
   }
